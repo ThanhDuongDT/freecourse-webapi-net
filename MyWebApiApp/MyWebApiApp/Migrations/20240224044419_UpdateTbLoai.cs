8d417bb562bd8127d0c9a2ad30fae47e0d841143
ThanhDuongDT <duongminhthanhdt@gmail.com>
2024-02-24 13:38:38 +0700

    Entity Framework Core (Code First)

---
+++ MyWebApiApp/MyWebApiApp/Migrations/20240224044419_UpdateTbLoai.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWebApiApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTbLoai : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TenLoai",
+                table: "Loai",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TenLoai",
+                table: "Loai",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
--- MyWebApiApp/MyWebApiApp/Program.cs
+++ MyWebApiApp/MyWebApiApp/Program.cs
@@ -1,3 +1,7 @@
+// configure services
+using Microsoft.EntityFrameworkCore;
+using MyWebApiApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(); // services.AddSwaggerGen();
+builder.Services.AddDbContext<MyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDB"));
+});
+// Configure middleware
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
